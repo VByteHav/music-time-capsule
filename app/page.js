3b7b6382be537d3294184ca4aa87da8a2efe41f0
VByteHav <vaibhavcodex11@gmail.com>
2025-02-16 10:01:04 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ const page = () => {
   <>
       <head>
         <meta property="og:title" content="Music Time Capsule - See your Music Journey" />
-        <meta property="og:description" content="Music Tiem capsule allows you to see your Last 4 Weeks, Last 6 Months and All Time music experience. It's a personal music tracker that helps you  stay connected with your favorite artists and tracks." />
+        <meta property="og:description" content="Music Time capsule allows you to see your Last 4 Weeks, Last 6 Months and All Time music experience. It's a personal music tracker that helps you  stay connected with your favorite artists and tracks." />
         <meta property="og:image" content="/favicon.png" />
         <meta property="og:url" content="https://music-time-capsule.vercel.app/" />
       </head>
